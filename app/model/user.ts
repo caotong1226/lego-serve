baf5a83382d487c8a13e85325eead9e6261125c0
caotong <>
2023-08-02 09:35:11 +0800

    feat: 权限校验

--- app/model/user.ts
+++ app/model/user.ts
@@ -2,45 +2,45 @@ import { Application } from 'egg';
 import { Schema } from 'mongoose';
 import * as AutoIncrementFactory from 'mongoose-sequence';
 export interface UserProps {
-    username: string;
-    password: string;
-    email?: string;
-    nickName?: string;
-    picture?: string;
-    phoneNumber?: string;
-    createdAt: Date;
-    updatedAt: Date;
-    type: 'email' | 'cellphone' | 'oauth';
-    provider?: 'gitee';
-    oauthID?: string;
-    role?: 'admin' | 'normal';
+  username: string;
+  password: string;
+  email?: string;
+  nickName?: string;
+  picture?: string;
+  phoneNumber?: string;
+  createdAt: Date;
+  updatedAt: Date;
+  type: 'email' | 'cellphone' | 'oauth';
+  provider?: 'gitee';
+  oauthID?: string;
+  role?: 'admin' | 'normal';
 }
 
 function initUserModel(app: Application) {
-    const AutoIncrement = AutoIncrementFactory(app.mongoose);
-    const UserSchema = new Schema<UserProps>({
-        username: { type: String, unique: true, required: true },
-        password: { type: String },
-        nickName: { type: String },
-        picture: { type: String },
-        email: { type: String },
-        phoneNumber: { type: String },
-        type: { type: String, default: 'email' },
-        provider: { type: String },
-        oauthID: { type: String },
-        role: { type: String, default: 'normal' },
-    }, {
-        timestamps: true,
-        toJSON: {
-            transform(_doc, ret) {
-                delete ret.password;
-                delete ret.__v;
-            },
-        },
-    });
-    //添加自增id
-    UserSchema.plugin(AutoIncrement, { inc_field: 'id', id: 'users_id_counter' });
-    return app.mongoose.model<UserProps>('User', UserSchema);
+  const AutoIncrement = AutoIncrementFactory(app.mongoose);
+  const UserSchema = new Schema<UserProps>({
+    username: { type: String, unique: true, required: true },
+    password: { type: String },
+    nickName: { type: String },
+    picture: { type: String },
+    email: { type: String },
+    phoneNumber: { type: String },
+    type: { type: String, default: 'email' },
+    provider: { type: String },
+    oauthID: { type: String },
+    role: { type: String, default: 'normal' },
+  }, {
+    timestamps: true,
+    toJSON: {
+      transform(_doc, ret) {
+        delete ret.password;
+        delete ret.__v;
+      },
+    },
+  });
+    // 添加自增id
+  UserSchema.plugin(AutoIncrement, { inc_field: 'id', id: 'users_id_counter' });
+  return app.mongoose.model<UserProps>('User', UserSchema);
 }
 
 export default initUserModel;
