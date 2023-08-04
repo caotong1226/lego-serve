7c3fca2e546b05de05298119a12890d9f1244785
caotong <>
2023-08-04 16:18:27 +0800

    feat: 去掉sharp

--- Dockerfile
+++ Dockerfile
@@ -3,8 +3,8 @@ RUN mkdir -p /usr/src/app
 WORKDIR /usr/src/app
 # 安装提前，缓存npm包，否则每次业务代码更改都会使缓存失效
 COPY package.json package-lock.json /usr/src/app/
-RUN npm config set sharp_binary_host "https://npm.taobao.org/mirrors/sharp"
-RUN npm config set sharp_libvips_binary_host "https://npm.taobao.org/mirrors/sharp-libvips"
+# RUN npm config set sharp_binary_host "https://npm.taobao.org/mirrors/sharp"
+# RUN npm config set sharp_libvips_binary_host "https://npm.taobao.org/mirrors/sharp-libvips"
 RUN npm install
 COPY . /usr/src/app/
 RUN npm run tsc
--- app/controller/utils.ts
+++ app/controller/utils.ts
@@ -1,6 +1,7 @@
 import { Controller, FileStream } from 'egg';
-import * as sharp from 'sharp';
-import { parse, join, extname } from 'path';
+// import * as sharp from 'sharp';
+import { join, extname } from 'path';
+// import { parse, join, extname } from 'path';
 import { nanoid } from 'nanoid';
 import { createWriteStream } from 'fs';
 import { pipeline } from 'stream/promises';
@@ -121,72 +122,72 @@ export default class UtilsController extends Controller {
     const results = await this.uploadFileUseBusBoy();
     ctx.helper.success({ ctx, res: results });
   }
-  async fileLocalUpload() {
-    const { ctx, app } = this;
-    const { filepath } = ctx.request.files[0];
-    // 生成sharp实例
-    const imageSource = sharp(filepath);
-    const metadata = await imageSource.metadata();
-    app.logger.debug(metadata);
-    let thumbnailUrl = '';
-    // 检查图片宽度是否大于300
-    if (metadata.width && metadata.width > 300) {
-      // 生成一个新的file path
-      // uploads/*/a.png => uploads/**/a-thumbnail.png
-      const { name, ext, dir } = parse(filepath);
-      app.logger.debug(name, ext, dir);
-      const thumbnailFilePath = join(dir, `${name}-thumbnail${ext}`);
-      await imageSource.resize({ width: 300 }).toFile(thumbnailFilePath);
-      thumbnailUrl = this.pathToURL(thumbnailFilePath);
-    }
-    const url = filepath.replace(app.config.baseDir, app.config.baseUrl);
-    ctx.helper.success({
-      ctx,
-      res: { url, thumbnailUrl: thumbnailUrl ? thumbnailUrl : url },
-    });
-  }
+  // async fileLocalUpload() {
+  //   const { ctx, app } = this;
+  //   const { filepath } = ctx.request.files[0];
+  //   // 生成sharp实例
+  //   const imageSource = sharp(filepath);
+  //   const metadata = await imageSource.metadata();
+  //   app.logger.debug(metadata);
+  //   let thumbnailUrl = '';
+  //   // 检查图片宽度是否大于300
+  //   if (metadata.width && metadata.width > 300) {
+  //     // 生成一个新的file path
+  //     // uploads/*/a.png => uploads/**/a-thumbnail.png
+  //     const { name, ext, dir } = parse(filepath);
+  //     app.logger.debug(name, ext, dir);
+  //     const thumbnailFilePath = join(dir, `${name}-thumbnail${ext}`);
+  //     await imageSource.resize({ width: 300 }).toFile(thumbnailFilePath);
+  //     thumbnailUrl = this.pathToURL(thumbnailFilePath);
+  //   }
+  //   const url = filepath.replace(app.config.baseDir, app.config.baseUrl);
+  //   ctx.helper.success({
+  //     ctx,
+  //     res: { url, thumbnailUrl: thumbnailUrl ? thumbnailUrl : url },
+  //   });
+  // }
   pathToURL(path: string) {
     const { app } = this;
     return path.replace(app.config.baseDir, app.config.baseUrl);
   }
-  async fileUploadByStream() {
-    const { ctx, app } = this;
-    const stream = await ctx.getFileStream();
-    const uid = nanoid(6);
-    const savedFilePath = join(
-      app.config.baseDir,
-      'uploads',
-      uid + extname(stream.filename),
-    );
-    const savedThumbnailFilePath = join(
-      app.config.baseDir,
-      'uploads',
-      uid + '_thumbnail' + extname(stream.filename),
-    );
+  // async fileUploadByStream() {
+  //   const { ctx, app } = this;
+  //   const stream = await ctx.getFileStream();
+  //   const uid = nanoid(6);
+  //   const savedFilePath = join(
+  //     app.config.baseDir,
+  //     'uploads',
+  //     uid + extname(stream.filename),
+  //   );
+  //   const savedThumbnailFilePath = join(
+  //     app.config.baseDir,
+  //     'uploads',
+  //     uid + '_thumbnail' + extname(stream.filename),
+  //   );
 
-    // 处理保存原始文件的流
-    const saveTarget = createWriteStream(savedFilePath);
-    const savePromise = pipeline(stream, saveTarget);
+  //   // 处理保存原始文件的流
+  //   const saveTarget = createWriteStream(savedFilePath);
+  //   const savePromise = pipeline(stream, saveTarget);
 
-    // 创建sharp实例并调整大小
-    const transformer = sharp().resize({ width: 300 });
+  //   // 创建sharp实例并调整大小
+  //   const transformer = sharp().resize({ width: 300 });
 
-    // 处理保存缩略图文件的流
-    const thumbnailTarget = createWriteStream(savedThumbnailFilePath);
-    const thumbnailPromise = pipeline(stream, transformer, thumbnailTarget);
+  //   // 处理保存缩略图文件的流
+  //   const thumbnailTarget = createWriteStream(savedThumbnailFilePath);
+  //   const thumbnailPromise = pipeline(stream, transformer, thumbnailTarget);
 
-    try {
-      await Promise.all([ savePromise, thumbnailPromise ]);
-    } catch (error) {
-      return ctx.helper.error({ ctx, errorType: 'imageUploadFail' });
-    }
+  //   try {
+  //     await Promise.all([ savePromise, thumbnailPromise ]);
+  //   } catch (error) {
+  //     return ctx.helper.error({ ctx, errorType: 'imageUploadFail' });
+  //   }
 
-    ctx.helper.success({
-      ctx,
-      res: {
-        url: this.pathToURL(savedFilePath),
-        thumbnailUrl: this.pathToURL(savedThumbnailFilePath),
-      },
-    });
-  }
+  //   ctx.helper.success({
+  //     ctx,
+  //     res: {
+  //       url: this.pathToURL(savedFilePath),
+  //       thumbnailUrl: this.pathToURL(savedThumbnailFilePath),
+  //     },
+  //   });
+  // }
 }
